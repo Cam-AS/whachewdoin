9744791739284659b6e9efdcdd146e66593e2770
Cameron Kirkpatrick <cameronk@appstrax.tech>
2021-07-09 23:43:20 +0200

    again

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,11 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    const { user } = this.props;
-    console.log(user);
+    // const { user } = this.props;
 
     return (
       <div>
-        Aweeeeh {user.email}
+        aweeeeee
       </div>
     )
   }
